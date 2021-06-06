323cc237dab3524edd8400be7d5b9085894dd3ac
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-06-06 16:41:05 -0300

    excercise 3 complete

--- java/ejercicios/level1/Ejer3.java
+++ java/ejercicios/level1/Ejer3.java
@@ -1,8 +1,18 @@
 package ejercicios.level1;
 
+import java.util.Scanner;
+
 public class Ejer3 {
     public static void main(String[] args) {
-        
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese numero: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        for (int i=1; i<=num; i++){
+            for (int x=1; x<=i; x++){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
     }    
 }
