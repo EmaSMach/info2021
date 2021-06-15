7e6af5585aa09eac01c51c38a93bb665f68c6fca
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2021-06-14 21:54:12 -0300

    removed unused imports

--- java/ejercicios/propuestos/Ejer5.java
+++ java/ejercicios/propuestos/Ejer5.java
@@ -1,10 +1,5 @@
 package ejercicios.propuestos;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 // import java.time.LocalDate;
